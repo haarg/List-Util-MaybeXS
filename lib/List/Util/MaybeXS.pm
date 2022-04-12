236bec21ded588ace9096c56f283e2c99901014c
Graham Knop <haarg@haarg.org>
2022-04-12 11:16:14 +0200

    Bumping version to 1.500008

--- lib/List/Util/MaybeXS.pm
+++ lib/List/Util/MaybeXS.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Exporter (); BEGIN { *import = \&Exporter::import }
 
-our $VERSION = '1.500_007';
+our $VERSION = '1.500008';
 $VERSION =~ tr/_//d;
 
 our @EXPORT_OK;
